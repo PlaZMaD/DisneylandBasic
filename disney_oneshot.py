b5d6d30ab273150080e8046a57d0d1b953e97671
PlaZMaD <lelituser@gmail.com>
2018-12-28 20:59:43 +0300

    some changes

--- disney_oneshot.py
+++ disney_oneshot.py
@@ -22,10 +22,9 @@ STATUS_FINAL = set([
     Job.COMPLETED,
     Job.FAILED,
 ])
-
-jobsNum = 10
-# nEvents_in = 6240925
-nEvents_in = 100
+exludeList = """
+"""
+exludeList = [int(i)-1 for i in exludeList.split()]
 
 def get_result(jobs):
     results = []
@@ -45,16 +44,23 @@ def CreateMetaData(tag):
     return json.dumps(metadata)
 
 
-def CreateJobInput(number):
+def CreateJobInput(number, options):
     job = copy.deepcopy(config.JOB_TEMPLATE)
     job['container']['cmd'] = job['container']['cmd'].format(
             job_id=number+1,
             IMAGE_TAG=IMAGE_TAG,
-            mfirstEvent=int(float(number*(nEvents_in/jobsNum))),
-	   # nEvents=17786274/
-            nEvents=int(float(nEvents_in/jobsNum + (((number + 1) == jobsNum) and [nEvents_in%jobsNum][0] or 0) ))
+            #mfirstEvent=int(float(number*(nEvents_in/jobsNum))),
+            mfirstEvent = options['startPoints'][number],
+            # mfirstEvent = startPoints[number],
+            #nEvents=int(float(nEvents_in/jobsNum + (((number + 1) == jobsNum) and [nEvents_in%jobsNum][0] or 0) ))
+            # nEvents = chunkLength[number]
+            nEvents = options['chunkLength'][number],
+            fileName = 'pythia8_Geant4_10.0_withCharmandBeauty' + str(int(options['fileN']*1000)) + '_mu.root'
         )
-    job['required_outputs']['output_uri'] = job['required_outputs']['output_uri'].format(job_id=number+1)
+    job['required_outputs']['output_uri'] = job['required_outputs']['output_uri'].format(job_id=number+1, fileName=str(options['fileN']))
+    job['input'][0] = job['input'][0].format(fileName = 'pythia8_Geant4_10.0_withCharmandBeauty' + str(int(options['fileN']*1000)) + '_mu.root')
+    with open('logs/'+str(number)+'.json', 'w') as outfile:
+        json.dump(job, outfile)
 
     return json.dumps(job)
 
@@ -98,14 +104,14 @@ def WaitForCompleteness(jobs, tag, verbose=False):
     return jobs_results
 
 
-def makeRun(tag, verbose=False):
+def makeRun(options, tag, verbose=False):
     jobs = [
         stub.CreateJob(Job(
-            input=CreateJobInput(i),
+            input=CreateJobInput(i, options),
             kind='docker',
             metadata=CreateMetaData(tag)
         ))
-        for i in range(jobsNum)
+        for i in range(options['jobsNum']) if i not in exludeList
 	#for i in [1, 4, 9, 10, 16, 17, 21, 24, 27, 42, 56, 64, 85, 94, 96]
     ]
 
@@ -116,17 +122,40 @@ def makeRun(tag, verbose=False):
 
 
 def main():
+
+    fileLen = {0: 13450391, 16000: 6242698, 66000: 6112412, 27000: 6238416, 63000: 6242811, 21000: 6236055, 34000: 6241933, 57000: 6240829, 10000: 6237695, 23000: 6234706, 26000: 6241631, 15000: 6245846, 60000: 6239611, 58000: 6235854, 29000: 6237372, 31000: 6238463, 39000: 6244654, 4000: 6237671, 22000: 2110646, 44000: 2793980, 59000: 6235770, 9000: 6239653, 47000: 6236843, 36000: 6239944, 14000: 6239735, 5000: 6238535, 45000: 6238019, 51000: 6242407, 41000: 6240065, 19000: 6234737, 49000: 6238063, 55000: 6240257, 33000: 6240302, 8000: 6239151, 20000: 6238057, 25000: 6236993, 61000: 6236622, 13000: 6244139, 38000: 6239812, 52000: 6243558, 30000: 6238488, 2000: 6238126, 3000: 6239983, 43000: 6239453, 28000: 6234302, 7000: 6246430, 53000: 6237809, 35000: 6238593, 56000: 6239181, 12000: 6239670, 18000: 6242505, 48000: 6238654, 54000: 6240632, 1000: 6240925, 62000: 3702269, 42000: 6242558, 64000: 6239932, 32000: 3881168, 6000: 6242602, 17000: 6243827, 40000: 6237918, 24000: 6238708, 50000: 5520395, 11000: 6238705, 65000: 6239301, 37000: 6238285, 46000: 6240834}
+    
     # uncompleted_jobs = []
     parser = argparse.ArgumentParser(description='Start run')
-    # parser.add_argument(
-    #     '--seed',
-    #     help='Random seed of simulation',
-    #     default=1
-    # )
-    # args = parser.parse_args()
+    parser.add_argument(
+        '--fileN',
+        type=int,
+        help='file #  for muonback input',
+        default=0
+    )
+    parser.add_argument(
+        '--jobsNum',
+        type=int,
+        help='Number of jobs',
+        default=1
+    )
+    args = parser.parse_args()
+
+    # jobsNum = 100
+    jobsNum = args.jobsNum
+    
+    nEvents_in = fileLen[args.fileN*1000]
+    #nEvents_in = 100
+    n = nEvents_in
+    k = jobsNum
+    startPoints = [i * (n // k) + min(i, n % k) for i in range(k)]
+    chunkLength = [(n // k) + (1 if i < (n % k) else 0) for i in range(k)]
+    chunkLength[-1] = chunkLength[-1] - 1
+
+    my_options = {'startPoints':startPoints, 'chunkLength':chunkLength, 'fileN':args.fileN, 'jobsNum':jobsNum} 
     tag = f'{RUN}_oneshot'
 
-    print("result:", makeRun(tag=tag, verbose=True))
+    print("result:", makeRun(my_options, tag=tag, verbose=True))
 
 
 if __name__ == '__main__':
